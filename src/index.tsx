fbe77be1a88aa4755096eb0b072efe8d9baa6d08
Oleg Konan <o.konan@softteco.com>
2022-01-04 14:39:47 +0300

    change layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { ScopedCssBaseline } from '@mui/material';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,9 +8,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScopedCssBaseline>
+      <StyledEngineProvider injectFirst>
         <App />
-      </ScopedCssBaseline>
+      </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,47 +1,37 @@
-import { AppBar, IconButton, List, ListItem, Typography } from '@mui/material';
 import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Typography from '@mui/material/Typography';
 import { Link, Outlet } from 'react-router-dom';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuIcon from '@mui/icons-material/Menu';
 import HelpOutlineSharpIcon from '@mui/icons-material/HelpOutlineSharp';
-import { Box } from '@mui/system';
+import styles from './styles.module.css';
 
 export const Layout: FC = () => {
   return (
-    <Box display="grid" component="main">
-      <AppBar position="static">
-        <Box display="flex" justifyContent="space-between" p={1}>
-          <IconButton>
-            <FastfoodIcon />
-          </IconButton>
-          <Typography variant="h6" component="h1">
-            Мои 100 желаний
-          </Typography>
-          <IconButton>
-            <HelpOutlineSharpIcon />
-          </IconButton>
-        </Box>
-      </AppBar>
-      <Box component="aside" sx={{ width: '10%', backgroundColor: '#E1BB80' }}>
-        <List component="nav">
-          <ListItem>
-            <Link to="/">Main</Link>
-          </ListItem>
-          <ListItem>
-            <Link to="game">Game</Link>
-          </ListItem>
-        </List>
+    <Box className={styles.layout}>
+      <Box component="header" className={styles.header}>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="h1">
+          Мои 100 желаний
+        </Typography>
+        <IconButton>
+          <HelpOutlineSharpIcon />
+        </IconButton>
       </Box>
+      <List component="nav" className={styles.navigation}>
+        <ListItem>
+          <Link to="/">Main</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="game">Game</Link>
+        </ListItem>
+      </List>
       <Outlet />
-      <Box component="aside" sx={{ width: '10%', backgroundColor: '#E1BB80' }}>
-        <List component="nav">
-          <ListItem>
-            <Link to="/">Main</Link>
-          </ListItem>
-          <ListItem>
-            <Link to="game">Game</Link>
-          </ListItem>
-        </List>
-      </Box>
     </Box>
   );
 };
