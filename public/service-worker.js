3fd62f7185a9af1e813d87f29281cbf0243931b0
Oleg Konan <o.konan@softteco.com>
2022-01-16 23:52:24 +0300

    fix servise worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,43 @@
 /* eslint-disable no-restricted-globals */
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = '/offline';
+const CACHE_NAME = 'app-cache-v1';
+const assetUrls = ['/', '/static/js/bundle.js', 'offline.html'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntill(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
-    })()
-  );
-  self.skipWaiting();
+self.addEventListener('install', async () => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(assetUrls);
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    (async () => {
-      if ('navigationPreload' in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
+self.addEventListener('activate', async () => {
+  const cachesNames = await caches.keys();
+  await Promise.all(
+    cachesNames
+      .filter((name) => name !== CACHE_NAME)
+      .map((name) => caches.delete(name))
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          console.log('Fetch failed; returning offline page instead.', error);
-
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
-        }
-      })()
-    );
+  const { request } = event;
+  const url = new URL(request.url);
+  if (url.origin === request.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
   }
 });
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? (await fetch(request));
+}
+async function networkFirst(request) {
+  const cache = await caches.open('dynamic');
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    const cached = await cache.match(request);
+    return cached ?? caches.match('/offline.html');
+  }
+}
